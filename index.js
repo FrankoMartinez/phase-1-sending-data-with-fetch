6084c3647ffe49410fc537debe74e39733865620
Francisco <frankmartinez4050@gmail.com>
2021-09-15 20:45:47 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json",
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(resp => resp.json())
+    .then(obj => {
+        let text = document.createElement('p')
+        console.log(text)
+        text.innerHTML = obj.id
+        console.log(obj)
+        document.querySelector('body').appendChild(text)
+    })
+    .catch(error => {
+        let p = document.createElement('p')
+        p.innerHTML = error.message
+        document.querySelector('body').appendChild(p)
+    })
+}
+submitData("name", "email")
